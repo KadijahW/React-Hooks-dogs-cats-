3b29edd098daf0c9776442432f7b5b532fb92d43
kadjiah <kadijahwilson@pursuit.org>
2020-01-25 14:25:59 -0500

    created dog component that gets 1 dog and takes in the input to get selected number of dogs

--- react-hooks-routing-lab/src/Dog.js
+++ react-hooks-routing-lab/src/Dog.js
@@ -1,1 +1,44 @@
-import React, {}
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+const Dog = () => {
+const [numofdogs, setnumofdogs] = useState(1);
+const [image, setImage] = useState([]);
+
+useEffect((e) => {
+    console.log("functional component did update!")
+    // setnumofdogs(e.target.value)
+})
+
+const getImage = async () => {
+    const response = await axios.get(`https://dog.ceo/api/breeds/image/random/${numofdogs}`)
+    console.log(response.data.message)
+    setImage(response.data.message)
+}
+
+useEffect(() => {
+    getImage()
+}, [])
+
+const handleDogAmount = (e) =>{
+setnumofdogs(e.target.value)
+}
+
+return(
+<div>
+    <h3>Dog Search</h3>
+    <input type="text" value={numofdogs} onChange={handleDogAmount} placeholder="Number of Dogs" />
+    <button onClick={getImage}>Get Dogs</button>
+    <br/>
+   
+   <ul>
+  { image.map(element => {
+       return <img src={`${element}`} alt="dog" />
+  })}
+    </ul>
+</div>
+)
+}
+
+export default Dog;
